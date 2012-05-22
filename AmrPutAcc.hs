27a1a6a65ed577d4c6692423981d97f8a020d7e0
Ken Friis Larsen <ken@friislarsen.net>
2012-05-22 13:01:26 +0200

    Add version for Accelerate. Although this version seems to make Accelerate cry...

---
+++ AmrPutAcc.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module AmrPutAcc 
+--       (binom, main)
+where
+import qualified Data.Array.Accelerate as A
+import qualified Data.Vector.Storable as V
+
+import Data.Array.Accelerate (Z(..), (:.)(..),(!))
+
+import qualified Data.Array.Accelerate.Interpreter as AI
+import qualified Data.Array.Accelerate.CUDA as ACUDA
+import qualified Data.Array.Accelerate.IO as AIO
+
+import Data.List(foldl')
+import System.Environment(getArgs)
+
+--import qualified Criterion.Main as C
+
+
+-- Pointwise manipulation of vectors an scalars
+v1 ^*^ v2 = A.zipWith (*) v1 v2
+v1 ^+^ v2 = A.zipWith (+) v1 v2
+c -^ v = A.map (c -) v
+c *^ v = A.map (c *) v
+
+pmax v c = A.map (A.max c) v
+ppmax = A.zipWith A.max 
+
+vtail = A.tail
+vinit = A.init
+vtake i v = A.take (A.constant i) v
+vdrop i v = A.drop (A.constant i) v
+
+vreverse v = 
+  let len = A.unindex1 (A.shape v) in
+  A.backpermute (A.shape v) (\ix -> A.index1 $ len - (A.unindex1 ix) - 1) v
+
+
+binom :: Int -> A.Acc(A.Vector Float)
+binom expiry = first --(first ! (A.constant 0))
+  where 
+    uPow, dPow :: A.Acc(A.Vector Float)
+    uPow = A.use $ AIO.fromVector $ V.generate (n+1) (u^)
+    dPow = A.use $ AIO.fromVector $ V.reverse $ V.generate (n+1) (d^)
+    
+    --uPow = A.generate (A.index1$ A.constant $ n+1) (\ix -> let i = A.unindex1 ix in u^i)
+    --dPow = vreverse $ A.generate (A.index1$ A.constant $ n+1) (\ix -> let i = A.unindex1 ix in d^i)
+    
+    st = s0 *^ (uPow ^*^ dPow)
+    finalPut = pmax (strike -^ st) 0
+
+-- for (i in n:1) {
+--   St<-S0*u.pow[1:i]*d.pow[i:1]
+--   put[1:i]<-pmax(strike-St,(qUR*put[2:(i+1)]+qDR*put[1:i]))
+-- }
+    first = foldl' prevPut finalPut [n, n-1 .. 1]
+    prevPut :: A.Acc(A.Vector Float) -> Int -> A.Acc(A.Vector Float)
+    prevPut put i = 
+      ppmax(strike -^ st) ((qUR *^ vtail put) ^+^ (qDR *^ vinit put))
+        where st = s0 *^ ((vtake i uPow) ^*^ (vdrop (n+1-i) dPow))
+
+    -- standard econ parameters
+    strike = 100
+    bankDays = 252
+    s0 = 100 
+    r = 0.03; alpha = 0.07; sigma = 0.20
+
+    n :: Int
+    n = expiry*bankDays
+    dt = fromIntegral expiry/fromIntegral n
+    u = exp(alpha*dt+sigma*sqrt dt)
+    d = exp(alpha*dt-sigma*sqrt dt)
+    stepR = exp(r*dt)
+    q = (stepR-d)/(u-d)
+    qUR = A.constant$ q/stepR; qDR = A.constant$ (1-q)/stepR
+
+
+arun run x = head $ A.toList $ run x
+
+-- main = do
+--   args <- getArgs
+--   case args of
+--     ["-f"] -> print $ binom 64
+--     _ -> do
+--       let benchmarks = [ C.bench (show years) $ C.nf binom years
+--                        | years <- [1, 16, 30, 32]]
+--       C.defaultMain benchmarks
